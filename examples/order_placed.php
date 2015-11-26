2f951bccb6e4dcaab2d2d0eeae06de9c4952353d
twalder-docnet <tom@doc-net.com>
2015-11-26 22:04:04 +0000

    Typo fix in demo

--- examples/order_placed.php
+++ examples/order_placed.php
@@ -5,6 +5,6 @@
 require(__DIR__ . '/../vendor/autoload.php');
 
 $obj_client = new \Venditan\Rapport\Client('test', 'api-key-goes-here-121gw');
-$obj_client->addUser()->id('1955')->name('MArty')->email('marty@mcfly.com')->mobile('07019551985');
+$obj_client->addUser()->id('1955')->name('Marty')->email('marty@mcfly.com')->mobile('07019551985');
 $obj_client->addTransaction()->id('2015');
 $obj_client->event('order_placed')->send();
