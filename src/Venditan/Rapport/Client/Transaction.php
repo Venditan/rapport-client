849a2227f8b5a96d30d38a8527ecd4cf957b6d95
twalder-docnet <tom@doc-net.com>
2015-12-01 09:41:25 +0000

    Added courier and tracking fields

--- src/Venditan/Rapport/Client/Transaction.php
+++ src/Venditan/Rapport/Client/Transaction.php
@@ -14,6 +14,10 @@ class Transaction
      * @var null|string
      */
     private $str_id = null;
+    
+    private $str_tracking_number = null;
+    
+    private $str_courier = null;
 
     /**
      * Set the user id
@@ -28,6 +32,30 @@ class Transaction
     }
 
     /**
+     * Set the tracking reference
+     *
+     * @param $str_tracking
+     * @return $this
+     */
+    public function tracking($str_tracking)
+    {
+        $this->str_tracking_number = $str_tracking;
+        return $this;
+    }
+
+    /**
+     * Set the courier used to ship the order
+     *
+     * @param $str_courier
+     * @return $this
+     */
+    public function courier($str_courier)
+    {
+        $this->str_courier = $str_courier;
+        return $this;
+    }
+
+    /**
      * Compile the data for transmission
      *
      * @return \stdClass
@@ -37,6 +65,12 @@ class Transaction
         $obj_data = new \stdClass();
         if(null !== $this->str_id) {
             $obj_data->order_id = $this->str_id;
+        }
+        if(null !== $this->str_tracking_number) {
+            $obj_data->tracking = $this->str_tracking_number;
+        }
+        if(null !== $this->str_courier) {
+            $obj_data->courier = $this->str_courier;
         }
         return $obj_data;
     }
